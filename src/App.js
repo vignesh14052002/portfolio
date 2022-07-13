7b88f13744701527988a1f0db9194037e5a4c9ae
vignesh14052002 <vignesharivazhagan1409@gmail.com>
2022-07-13 15:12:36 +0530

    myportfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+      <div class="logo">
+        <div class="firstletter">V</div>
+        <div class="remaining">icky</div>
+      </div>
+      <a href="#" class="mobilemenu">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a>
+      <div class="sections"  active="false">
+        <div>Home</div>
+        <div>Blog</div>
+        <div>Gallery</div>
+        <div>About</div>
+        
+      <a href="https://vignesh14052002.github.io/p5js-projects/empty-example/bubbles/index.html">Projects</a>
+ 
+      </div>
+      
+    </div>
+
     </div>
   );
 }
